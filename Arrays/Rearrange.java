6066ec2194df31c2d9380f2e7b4165ecd8a5082b
Mohammed Tajammul <76564671+Tajammul607@users.noreply.github.com>
2024-04-25 17:12:03 +0000

    updated

--- Arrays/Rearrange.java
+++ Arrays/Rearrange.java
@@ -1,5 +1,4 @@
 
-
 package Arrays;
 
 /**
@@ -7,25 +6,53 @@ package Arrays;
  */
 public class Rearrange {
 
-    //brute force 
-    public static void RearrangeEle(int arr[]){
+    // brute force
+    public static void RearrangeEle(int arr[]) {
 
         // [1,2,3,4,5,6] ==> 6,1,5,2,4,3
         int newArr = new int[arr];
 
-        int start=0;
-        int end =arr.length-1;
+        int start = 0;
+        int end = arr.length - 1;
 
         while (start < end) {
-            if(arr[end] %2 ==0){
-                newArr=arr[end];
+            if (arr[end] % 2 == 0) {
+                newArr = arr[end];
                 end--;
-            }else {
+            } else {
                 newArr = arr[start];
                 start++;
             }
-            
+
         }
-    
+
+    }
+
+}
+
+public static void rearrange(long arr[], int n){
+    int min_idx = 0 , 
+    max_idx = n-1;
+    long max_elem = arr[max_idx]+1;
+
+    for(int i=0; i<n; i++){
+        if (i % 2 == 0){
+
+            arr[i] +=  arr[max_idx] % max_elem * max_elem;---5(index)%49 or 6(value)%49
+
+            max_idx--; 5-1=>4
+
+        }
+
+        else{
+            arr[i] +=  arr[min_idx] % max_elem * max_elem;
+            min_idx++;
+        }
+
+    }
+    for(int i=0; i<n; i++){
+
+        arr[i] = arr[i]/max_elem;
+        
     }
 }
